039ee9f8b770766c82dd2304b6f7f2dc143013df
Ali Ben-Hammad <Ali.Ben-Hammad@pipenile.com>
2024-10-18 21:12:05 +0100

    implement supplier and consumer to automate event processing

---
+++ src/main/java/org/example/kafkaspring/services/PageEventService.java
@@ -0,0 +1,32 @@
+package org.example.kafkaspring.services;
+
+import org.example.kafkaspring.entities.PageEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+@Service
+public class PageEventService {
+    @Bean
+    public Consumer<PageEvent> pageEventConsumer(){
+        return input -> {
+            System.out.println("*** PageEventConsumer ***");
+            System.out.println(input);
+            System.out.println("**************************");
+        };
+    }
+
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier(){
+        return ()-> new PageEvent(
+                Math.random()>0.5?"page1":"page2",
+                Math.random()>0.5?"user1":"user2",
+                new Date(),
+                new Random().nextInt(90000));
+    }
+}
+
